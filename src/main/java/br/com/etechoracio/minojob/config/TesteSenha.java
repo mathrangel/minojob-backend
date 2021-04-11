fcb76683e3d03ea97db96e6b26c5d7b7da6bbaf5
Matheus Rangel <Matheus.rangelanatovitz@gmail.com>
2021-04-11 16:28:45 -0300

    Modificações para Login e Autenticação [não finalizado]
    Criado método inicial de hash de senha e método inicial de token.

---
+++ src/main/java/br/com/etechoracio/minojob/config/TesteSenha.java
@@ -0,0 +1,10 @@
+package br.com.etechoracio.minojob.config;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TesteSenha {
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder(12).encode("admin"));
+    }
+    
+}
--- src/main/java/br/com/etechoracio/minojob/controllers/UsuarioController.java
+++ src/main/java/br/com/etechoracio/minojob/controllers/UsuarioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.etechoracio.minojob.model.Curriculo;
+import br.com.etechoracio.minojob.model.Usuario;
 import br.com.etechoracio.minojob.repository.UsuarioRepository;
 
 @RestController
@@ -17,7 +17,7 @@ public class UsuarioController {
 	private UsuarioRepository repository;
 	
 	@PostMapping
-	public Curriculo create(@RequestBody Curriculo body) {
+	public Usuario create(@RequestBody Usuario body) {
 		return repository.save(body);
 	}
 	
