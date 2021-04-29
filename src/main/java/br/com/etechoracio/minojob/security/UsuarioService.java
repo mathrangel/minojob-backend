bcb7e1c9492f1d894c9d26dbc7184ba500b4082a
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2021-04-29 18:47:14 -0300

    adicionando anotação @Service

--- src/main/java/br/com/etechoracio/minojob/security/UsuarioService.java
+++ src/main/java/br/com/etechoracio/minojob/security/UsuarioService.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import br.com.etechoracio.minojob.model.Usuario;
 import br.com.etechoracio.minojob.repository.UsuarioRepository;
 
-
+@Service
 public class UsuarioService implements UserDetailsService{
 
 	private UsuarioRepository repository;
