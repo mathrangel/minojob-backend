18d6f926c5573907724a8f20c8904505c9b7dc6a
Matheus Rangel <matheus.rangelanatovitz@gmail.com>
2021-06-08 19:09:58 -0300

    Método para pesquisar por nome

--- src/main/java/br/com/etechoracio/minojob/repository/VagaRepository.java
+++ src/main/java/br/com/etechoracio/minojob/repository/VagaRepository.java
@@ -1,10 +1,13 @@
 package br.com.etechoracio.minojob.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.etechoracio.minojob.model.Vaga;
 
 public interface VagaRepository extends JpaRepository<Vaga, Integer> {
 
+	List<Vaga> findByNome(String nome);
 	
 }
